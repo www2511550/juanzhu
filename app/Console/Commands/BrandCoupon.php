7099d5be709eaf38e4594efa72997b1337933122
程聪 <379624432@qq.com>
2021-01-29 10:37:06 +0800

    品牌特惠更新

--- app/Console/Commands/BrandCoupon.php
+++ app/Console/Commands/BrandCoupon.php
@@ -38,16 +38,81 @@ class BrandCoupon extends Command
      */
     public function handle()
     {
+
+
+//        $this->brand();
+
+        $this->brandCoupon();
+
+    }
+
+
+    /**
+     * 获取品牌数据
+     */
+    public function brand()
+    {
+        $url = 'http://onsales.top/dtk.php?act=brandName';
+        $strJson = file_get_contents($url);
+        $data = json_decode($strJson, true);
+        $log_CateID = 3; // 品牌id
+        $hours = -6;
+
+        $db = DB::connection('baiduMysql');
+        foreach ($data['data'] as $value){
+            $log_Title = $value['brandName'].'-'.$value['simpleLabel'];
+            $record = $db->table('post')->where('log_CateID', $log_CateID)->where('log_Title', $log_Title)->first();
+//            pre($value);
+//            pre($record);die;
+            $logCentent = '<p><img class="ue-image" src="' . $value['brandLogo'] . '" title="'.$value['brandName'].'.png" alt="'.$value['brandName'].'.png"/></p>';
+            $logCentent .= $value['brandDesc'];
+            if ($record){
+                pre($record->log_ID);
+                $db->table('post')->where('log_ID', $record->log_ID)->update([
+                    'log_Content' => $logCentent,
+                    'log_Meta' => $value['brandId'],
+                    'log_AuthorID' => 2,
+                    'log_ViewNums' => rand(1,100)
+                ]);
+            }else{
+                $hours -= 6;
+                $db->table('post')->insert([
+                    'log_CateID' => $log_CateID,
+                    'log_AuthorID' => 2,
+                    'log_Title' => $log_Title,
+                    'log_Intro' => $value['brandDesc'],
+                    'log_Content' => $logCentent,
+                    'log_PostTime' => strtotime(date("Y-m-d H:i:s", strtotime($hours." hours"))),
+                    'log_Meta' => $value['brandId'], // 大淘客品牌id
+                ]);
+            }
+        }
+
+    }
+
+    /**
+     * 获取品牌优惠券数据
+     */
+    public function brandCoupon()
+    {
         $db = DB::connection('baiduMysql');
         $dbData = $db->table('post')->where('log_CateID', '>', 2)->get();
 
-        foreach ($dbData as $record){
-            $data = json_decode(file_get_contents('http://onsales.top/dtk.php?act=brandProudct&id='.$record->log_Meta), true);
+        foreach ($dbData as $record) {
+            $data = json_decode(file_get_contents('http://onsales.top/dtk.php?act=brandProudct&id=' . $record->log_Meta), true);
             $strGoods = '';
             // 按照销量排序
             $arrList = $data['data']['list'] ?: [];
             $arrTmpPrice = array();
-            if (!count($arrList)) continue;
+            // 没有商品，更新品牌信息
+            if (!count($arrList)){
+                $strCover = !strpos($data['data']['brandLogo'], '//') ? ('https:' . $data['data']['brandLogo']) : $data['data']['brandLogo'];
+                $db->table('post')->where('log_ID', $record->log_ID)->update([
+                    'log_Cover' => $strCover,
+                    'log_BrandID' => $data['data']['brandId'],
+                ]);
+                continue;
+            }
             foreach ($arrList as $value) {
                 $arrTmpPrice[] = $value['monthSales'];
             }
@@ -62,7 +127,7 @@ class BrandCoupon extends Command
 //            $strGoods .= '<p><a href="' . $coupon['data']['couponClickUrl'] . '" target="_blank"><img class="ue-image" src="' . ($value['marketingMainPic'] ?: $value['mainPic']) . '" title="' . $value['title'] . '"/></a></p>
 //            <p><a href="' . $value['couponLink'] . '" target="_blank">【券后' . $value['actualPrice'] . '元】' . $value['desc'] . '【复制口令：'.$coupon['data']['tpwd'].'】</a></p>';
             $record->log_Content = $strGoods;
-            $strCover = !strpos($value['mainPic'], '//') ? ('https:'.$value['mainPic']) : $value['mainPic'];
+            $strCover = !strpos($value['mainPic'], '//') ? ('https:' . $value['mainPic']) : $value['mainPic'];
             echo $db->table('post')->where('log_ID', $record->log_ID)->update([
                 'log_Content' => $record->log_Content,
                 'log_Cover' => $strCover,
@@ -71,41 +136,5 @@ class BrandCoupon extends Command
             ]);
             usleep(100000);
         }
-
-//        // 品牌
-//        $url = 'http://onsales.top/dtk.php?act=brandName';
-//        $strJson = file_get_contents($url);
-//        $data = json_decode($strJson, true);
-//        $log_CateID = 3; // 品牌id
-//        $hours = -6;
-//        foreach ($data['data'] as $value){
-//            $log_Title = $value['brandName'].'-'.$value['simpleLabel'];
-//            $record = $db->table('post')->where('log_CateID', $log_CateID)->where('log_Title', $log_Title)->first();
-////            pre($value);
-////            pre($record);die;
-//            $logCentent = '<p><img class="ue-image" src="' . $value['brandLogo'] . '" title="'.$value['brandName'].'.png" alt="'.$value['brandName'].'.png"/></p>';
-//            $logCentent .= $value['brandDesc'];
-//            if ($record){
-//                pre($record->log_ID);
-//                $db->table('post')->where('log_ID', $record->log_ID)->update([
-//                    'log_Content' => $logCentent,
-//                    'log_Meta' => $value['brandId'],
-//                    'log_AuthorID' => 2,
-//                    'log_ViewNums' => rand(1,100)
-//                ]);
-//            }else{
-//                $hours -= 6;
-//                $db->table('post')->insert([
-//                    'log_CateID' => $log_CateID,
-//                    'log_AuthorID' => 2,
-//                    'log_Title' => $log_Title,
-//                    'log_Intro' => $value['brandDesc'],
-//                    'log_Content' => $logCentent,
-//                    'log_PostTime' => strtotime(date("Y-m-d H:i:s", strtotime($hours." hours"))),
-//                    'log_Meta' => $value['brandId'], // 大淘客品牌id
-//                ]);
-//            }
-//        }
-
     }
 }
